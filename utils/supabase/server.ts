a1a549d2bcf0240de622f1ccd3887c684ae1a575
HumanBuilds <human.builds.dev@gmail.com>
2025-12-29 20:12:56 +0000

    feat(supabase): enforce db schema based on env var

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -8,6 +8,9 @@ export async function createClient() {
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
+            db: {
+                schema: process.env.NEXT_PUBLIC_DB_SCHEMA,
+            },
             cookies: {
                 getAll() {
                     return cookieStore.getAll()
